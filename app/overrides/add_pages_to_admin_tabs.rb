a5e69e4e48d26934a6bcdd6ab6c5ee2c9d9be062
Nemo Fang <fyj123121@gmail.com>
2015-12-17 17:40:23 +0800

    Place static content menu in top tab menu in admin panel for easy access.

---
+++ app/overrides/add_pages_to_admin_tabs.rb
@@ -0,0 +1,6 @@
+Deface::Override.new(
+  virtual_path: "spree/admin/shared/_menu",
+  name: "static_content_tab",
+  insert_bottom: '[data-hook="admin_tabs"]',
+  partial: "spree/admin/pages/static_content_tab"
+)
